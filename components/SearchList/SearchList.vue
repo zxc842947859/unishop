929ccde091372ec46a716d39e2f497f9a6dc0358
chao <zhangxc1120@qq.com>
2020-12-25 18:19:29 +0800

    搜索界面实现

---
+++ components/SearchList/SearchList.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		搜索列表
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		async onLoad(option) {
+			const res = await this.$request({
+				url: '/goods/search',
+				data: {
+					query: option.kw,
+					pagenum: 1,
+					pagesize: 5
+				}
+			})
+			console.log(res)
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- components/SearchPage.vue
+++ components/SearchPage.vue
@@ -1,26 +1,17 @@
 <template>
 	<view class="search-page">
 		<view class="search-bar">
-			<input type="text" value="" placeholder="请输入您想要的商品" />
+			<input type="text" v-model="searchKeyWord" placeholder="请输入您想要的商品" @confirm="searchEvent" />
 			<icon type="search" size="36rpx"></icon>
 			<button type="default" size="mini">取消</button>
 		</view>
 		<view class="search-box">
 			<view class="top">
 				<text>历史搜索</text>
-				<icon type="clear" size="30rpx" color="#cccccc"></icon>
+				<icon type="clear" size="30rpx" color="#cccccc" @click="clearHistory"></icon>
 			</view>
 			<view class="search-list">
-				<text class="search-record">小米</text>
-				<text class="search-record">小米小米</text>
-				<text class="search-record">小米小米小米</text>
-				<text class="search-record">小米小</text>
-				<text class="search-record">小米小米小</text>
-				<text class="search-record">小米小米</text>
-				<text class="search-record">小米小米小米</text>
-				<text class="search-record">小米小</text>
-				<text class="search-record">小米小米小</text>
-				<text class="search-record">小米小米</text>
+				<text class="search-record" v-for="(item, index) in searchHistoryList" :key="index">{{ item }}</text>
 			</view>
 
 		</view>
@@ -31,7 +22,61 @@
 	export default {
 		data() {
 			return {
+				searchKeyWord: '', // 搜索关键字
+				searchHistoryList: []
+			}
+		},
+		onShow() {
+			uni.getStorage({
+				key: 'historyList',
+				success: (res) => {
+					this.searchHistoryList = res.data
+				}
+			})
+		},
+		methods: {
+			saveHistory({
+				key = 'historyList',
+				data
+			}) {
+				// 存储
+				uni.setStorage({
+					key: key,
+					data: data,
+					success: () => {}
+				})
+			},
+			// 搜索Í
+			searchEvent() {
+				// 添加搜索记录到列表开头
+				this.searchHistoryList.unshift(this.searchKeyWord)
+				this.searchHistoryList.length > 10 && (this.searchHistoryList = this.searchHistoryList.slice((0, 10)))
+				// 去重
+				this.searchHistoryList = [...new Set(this.searchHistoryList)]
+				console.log(this.searchHistoryList)
+				this.saveHistory({
+					data: this.searchHistoryList
+				})
+				// 跳转到搜索列表界面
+				uni.navigateTo({
+					url: '/components/SearchList/SearchList?kw=' + this.searchKeyWord
+				})
+			},
+			// 清除历史搜索记录
+			clearHistory() {
 
+				this.searchHistoryList.length && uni.showModal({
+					title: '提示',
+					content: '您确定要删除历史搜索记录嘛?',
+					success: (res) => {
+						res.confirm && uni.removeStorage({
+							key: 'historyList',
+							success: () => {
+								this.searchHistoryList = []
+							}
+						})
+					}
+				})
 			}
 		}
 	}
@@ -40,7 +85,6 @@
 <style lang="less" scoped>
 	.search-page {
 		.search-bar {
-			// height: 120rpx;
 			position: relative;
 			background: #eeeeee;
 			padding: 30rpx 16rpx;
@@ -83,25 +127,28 @@
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
+
 				text {
 					line-height: 88rpx;
 				}
+
 				icon {
 					margin-right: 16rpx;
 				}
 			}
+
 			.search-list {
 				display: flex;
 				padding: 0 16rpx;
 				justify-content: flex-start;
 				flex-wrap: wrap;
-				
+
 				.search-record {
 					font-size: 24rpx;
 					background: #dddddd;
 					padding: 10rpx 20rpx;
 					color: #333;
-					margin: 0 30rpx 20rpx 0 ;
+					margin: 0 30rpx 20rpx 0;
 				}
 			}
 		}
